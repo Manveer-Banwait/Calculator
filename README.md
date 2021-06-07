07262b8e1698b20bcdafc8a53387cda6691ecbcb
Manveer Banwait <Manveer.Banwait1@gmail.com>
2021-06-07 09:56:15 -0400

    finished main chunk of the calculations

--- README.md
+++ README.md
@@ -1,3 +1,78 @@
 # Calculator
 
+if (nums == editors[0]|| nums == editors[1]){
+        
+        editor(nums)
+    }
 
+    if (digits.includes(nums)){
+        if(isNaN(parseInt(nums))!==true){
+        accumulate.push(parseInt(nums))
+        }
+        else if(isNaN(parseInt(nums))===true){
+            accumulate.push(nums)
+        }
+        console.log(accumulate) 
+        input.innerText = accumulate.join('') 
+        
+    }
+
+    
+    else if (operands.includes(nums)){
+        operatorCount++//checks how many operators
+        operatorArray.push(nums) //tracks which operators were clicked
+        
+        if(accumulate.length>0){ //joins the accumulating numbers into one number
+            finalArray.push(accumulate.join(''))
+            parseFloat(finalArray);
+            console.log("test "+finalArray)
+            accumulate.splice(0,accumulate.length)
+        }
+        
+        
+        if(nums== '='){
+            accumulate.splice(0,accumulate.length) 
+            switchBoard()
+        }
+        if (operatorArray.length==2){
+            operatorArray.splice[0]
+            console.log("operators "+operatorArray)
+            accumulate.splice(0,accumulate.length)
+            switchBoard()
+            accumulate.push(input.innerText);
+            console.log("final array is "+finalArray)
+        }
+
+        
+    }
+
+
+
+
+
+
+
+
+    if (nums!=="="){
+            operatorArray.push(nums)
+        }
+
+        finalArray.push(parseFloat(accumulate.splice(0,accumulate.length-1.).join('')))
+        accumulate.splice(0,accumulate.length)
+        console.log(accumulate, finalArray)
+
+        if (nums == "="){
+            
+            switchBoard()
+        }
+
+        if (operatorArray.length==2){
+            console.log("operations"+operatorArray)
+            console.log(finalArray)
+            let test = (switchBoard())
+            accumulate.splice(0, accumulate.length)
+            accumulate.push(test);
+
+            console.log("nigga "+accumulate)
+        }
+    }
--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 const numButtons = document.getElementsByTagName('button');
-const input = document.getElementsByClassName('current')
+const input = document.getElementById('input')
 const output = document.getElementsByClassName('output')
 
 
-let operands = ['×','÷','+','-','='];
-let digits = ['1','2','3','4','5','6','7','8','9','0','.'];
-let accumulate = [];
+let operators = ['×','÷','+','-','='];
 
+let digits = ['1','2','3','4','5','6','7','8','9','0'];
+let editors = ['AC','←'];
+
+let point = '.';
+let pointCount = 0;
+
+let accumulate = [];
 let finalArray=[];
+
+let holder = 0;
 
 let operatorCount=0;
 let operatorArray = [];
+
+let answer = 0;
+
+let flag=false;
 
 Array.from(numButtons).forEach((button) => {
     button.addEventListener('click', ()=>{
@@ -19,64 +30,155 @@ Array.from(numButtons).forEach((button) => {
     
 });
 
-function calculate(input){
-
-    if (digits.includes(input)){
-        accumulate.push(parseInt(input))
-        console.log(accumulate) //when you get rid of this line, it doesn't work anymore.
+function calculate(nums){
+    if (nums == editors[0]|| nums == editors[1]){
+        
+        editor(nums)
+        return
     }
+
+    if (nums!=="="){
+        if(digits.includes(nums)){
+            accumulate.push(parseInt(nums))
+        }
+
+        if(nums == point && pointCount == 0){
+            pointCount++
+            accumulate.push(nums)
+        }
+       
+    }
+    console.log(accumulate)
 
     
-    else if (operands.includes(input)){
-        operatorCount++//checks how many operators
-        operatorArray.push(input) //tracks which operators were clicked
-        
-        if(accumulate.length>0){
-            finalArray.push(parseInt(accumulate.join('')))
-            
-            accumulate.splice(0,accumulate.length)
-            
-            console.log(finalArray);
+    if(operators.includes(nums)){
+
+
+        if(nums !== '='){
+            operatorCount++
+            console.log(operatorCount)
         }
         
+        if(accumulate.length!=0){
+        finalArray.push(parseInt(accumulate.join("")))
+        }
+        finalArray.push(nums)
+        accumulate.splice(0,accumulate.length)
+        console.log(finalArray)
+
         
-        if(operatorCount==2|| input== '='){
-            console.log(finalArray);
 
-            switch(operatorArray[0]){
-                case "-":
-                    subtract(finalArray)
-                    break;
-                case '+':
-                    add(finalArray)
+        
+        
+        if (nums == "="){
+            holder = finalArray.indexOf('=')
+            finalArray.splice(holder)
+            console.log(finalArray)
+            switchBoard()
+            accumulate.splice(0, accumulate.length)
 
-            }
         }
 
         
-        
+        if (operatorCount==2){
+            flag = true;
+            console.log(flag)
+            console.log(finalArray)
+            let test=(switchBoard());
+            accumulate.splice(0, accumulate.length)
+            console.log(finalArray)
+            console.log(operatorCount)
+        }
+    }
+
+}
 
 
+
+function switchBoard(){
+
+    
+    if(finalArray[1]=='-'){
+        input.innerText=(subtract(finalArray))
+
+        if(flag==false){
+        finalArray.splice(0,finalArray.length)
+        finalArray.unshift(parseFloat(input.innerText))
+        console.log(finalArray, accumulate.length)
+        operatorCount--
+        }
+
+        if(flag==true){
+           finalArray.splice(0, finalArray.length-1)
+           finalArray.unshift(parseFloat(input.innerText))
+           operatorCount--
+           flag=false
+       }
+        return input.innerText;
+      }  
+
+
+    
+    if(operatorArray[0]=='+'){
+        input.innerText=(add(finalArray))
+        finalArray.splice(0,finalArray.length)
+        operatorArray.splice(0)
+       
+    }
+    
+    if(operatorArray[0]=='×'){
+        input.innerText=(multiply(finalArray))
+        finalArray.splice(0,finalArray.length)
+        operatorArray.splice(0)
         
     }
-}
-
-
-
-function add (addd){
-    console.log(addd[0]+addd[1]) //NOTE: this is where we left off.
-}
-
-function subtract(yaddaz){
-    console.log(yaddaz[0]-yaddaz[1])
-}
-
-function multiply(a,b){
-    console.log(a*b)
+   
+    if(operatorArray[0]=='÷')
+        input.innerText=(divide(finalArray))
+        finalArray.splice(0,finalArray.length)
+        operatorArray.splice(0)
+        
 
 }
 
-function divide(a,b){
-    console.log(Math.floor(a/b))
+
+
+
+function add (adding){
+    console.log(parseInt(adding[0])+parseInt(adding[2]))
+
+}
+
+function subtract(subtracting){
+    return(parseInt(subtracting[0])-parseInt(subtracting[2]))
+    
+}
+
+function multiply(multiplying){
+   return multiplying[0]*multiplying[1]
+
+}
+
+function divide(dividing){
+    return dividing[0]/dividing[1]
+
+}
+
+function editor(selection){
+    if(selection == '←'){
+        accumulate.splice(accumulate.length-1)
+        console.log(accumulate)
+        console.log('success')
+        input.innerText = accumulate.join('')
+    }
+
+    if(selection == 'AC'){
+        accumulate.splice(0,accumulate.length)
+        finalArray.splice(0, finalArray.length)
+        input.innerText = accumulate.join('')
+
+    }
+
+
 
 }
